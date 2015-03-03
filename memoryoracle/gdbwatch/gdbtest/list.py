5933617d18814948693cce3dc8894cd61608a225
Daniel Noland <daniel.noland@gmail.com>
2015-03-03 16:39:28 -0700

    Fixed circular ref

--- memoryoracle/gdbwatch/gdbtest/list.py
+++ memoryoracle/gdbwatch/gdbtest/list.py
@@ -10,45 +10,77 @@ import pprint
 
 gdb.execute("start", False, False)
 
-def struct_to_dict(s):
-    if s.type.code == gdb.TYPE_CODE_STRUCT:
-        return { f.name: struct_to_dict(s[f.name]) for f in s.type.fields() }
-    else:
-        return s
+class MemoryState(object):
 
-def get_local_vars():
-    localString = gdb.execute("info locals", False, True)
-    transform = re.sub(r'([a-zA-Z_][0-9a-zA-Z_:]*) \= .*\n', r"'\1', ", localString)
-    transform = "(" + transform[:-2] + ")"
-    localVars = ast.literal_eval(transform)
-    gdbVars = { v: gdb.parse_and_eval(v) for v in localVars }
-    return gdbVars
+    def __init__(self):
+        self._pp = pprint.PrettyPrinter(indent=3)
+        self._valueDict = dict()
+        self._structDict = dict()
+        self._arrayDict = dict()
 
-def serialize_value(s):
-    s.fetch_lazy()
-    if s.type.code == gdb.TYPE_CODE_STRUCT:
-        serialized = { "address": str(s.address),
-                      "is_optomized_out": s.is_optimized_out,
-                      "type": s.type.name,
-                      "stripped_type": s.type.strip_typedefs().name,
-                      "dynamic_type": s.dynamic_type.name,
-                      "value": { f.name: serialize_value(s[f.name]) for f in s.type.fields() }
-                }
-        return serialized
-    elif s.type.code == gdb.TYPE_CODE_PTR:
-        try:
-            return serialize_value(s.dereference())
-        except gdb.MemoryError as e:
-            print(e) #DEBUG
-            if str(s) == "0x0":
-                return "nullptr"
+    @staticmethod
+    def struct_to_dict(s):
+        if s.type.code == gdb.TYPE_CODE_STRUCT:
+            return { f.name: MemoryState.struct_to_dict(s[f.name]) for f in s.type.fields() }
+        else:
+            return s
+
+    def _get_local_vars(self):
+        localString = gdb.execute("info locals", False, True)
+        transform = re.sub(r'([a-zA-Z_][0-9a-zA-Z_:]*) \= .*\n', r"'\1', ", localString)
+        if transform[-2:] is ", ":
+            transform = transform[:-2]
+        transform = "(" + transform + ")"
+        localVars = ast.literal_eval(transform)
+        self.gdbVars = { v: gdb.parse_and_eval(v) for v in localVars }
+
+    def _serialize_value(self, s, name):
+        addr = str(s.address)
+        if addr not in self._valueDict:
+            s.fetch_lazy()
+            self._valueDict[addr] = {
+                "name": name,
+                "is_optomized_out": s.is_optimized_out,
+                "type": s.type.name,
+                "stripped_type": s.type.strip_typedefs().name,
+                "dynamic_type": s.dynamic_type.name,
+            }
+            if s.type.code == gdb.TYPE_CODE_STRUCT:
+                self._structDict[addr] = self._valueDict.pop(addr)
+                for f in s.type.fields():
+                    self._serialize_value(s[f.name], name + "." + f.name)
+                self._valueDict[addr]["value"] = str(s.address)
+            elif s.type.code == gdb.TYPE_CODE_ARRAY:
+                self._arrayDict[addr] = self._valueDict.pop(addr)
+                self._arrayDict[addr]["range"] = s.type.range()
+                self._arrayDict[addr]["target_type"] = s.type.target().name
+                for i in range(s.type.range()[1] + 1):
+                    self._serialize_value(s[i], name + "[" + str(i) + "]" )
+            elif s.type.code == gdb.TYPE_CODE_PTR:
+                try:
+                    self._valueDict[addr]["value"] = str(s)
+                    self._serialize_value(s.dereference(), "(*" + name + ")")
+                except gdb.MemoryError as e:
+                    if str(s) == "0x0":
+                        self._valueDict[addr]["value"] = "nullptr"
+                    else:
+                        self._valueDict[addr]["value"] = str(e)
             else:
-                return "invalid"
-    else:
-        return str(s)
+                self._valueDict[addr]["value"] = str(s)
 
-def serialize_locals():
-    return { k: serialize_value(v) for k,v in get_local_vars().items() }
 
-pp = pprint.PrettyPrinter(indent=3)
-pp.pprint(serialize_locals())
+    def serialize_locals(self):
+        self._get_local_vars()
+        for k, v in self.gdbVars.items():
+            self._serialize_value(v, k)
+
+        return (self._valueDict, self._structDict, self._arrayDict)
+
+    def __str__(self):
+        self._pp.pprint(self.serialize_locals())
+
+# pp.pprint(serialize_locals())
+m = MemoryState()
+state = m.serialize_locals()
+m._pp.pprint(state)
+
--- memoryoracle/gdbwatch/gdbtest/new.cpp
+++ memoryoracle/gdbwatch/gdbtest/new.cpp
@@ -1,26 +1,35 @@
+struct MyStruct;
+struct MyOtherStruct;
+
 struct MyStruct {
    int* a;
    int b;
+   MyOtherStruct* c;
 };
 
 struct MyOtherStruct {
    MyStruct* a;
    int b;
+   MyOtherStruct* cc;
 };
 
 int main(int argc, char *argv[]) {
-   int* a;
-   int* b = new int[10];
-   int c = 3;
+   /* int* a; */
+   /* int* b = new int[10]; */
+   /* int c = 3; */
+   int a[5] = { 1, 2, 3, 4, 5 };
+   float b[2][2] = { {1 ,2}, {3, 4} };
    MyOtherStruct exx;
    exx.a = new MyStruct[10];
    exx.b = 2;
    exx.a[0] = MyStruct();
    exx.a[0].b = 3;
-   exx.a[1].a = &c;
-   a = exx.a[1].a;
-   *a = 42;
-   b = nullptr;
+   exx.cc = &exx;
+   exx.a[0].c = &exx;
+   /* exx.a[1].a = &c; */
+   /* a = exx.a[1].a; */
+   /* *a = 42; */
+   /* b = nullptr; */
    delete[] exx.a;
    return 0;
 }
